3b7148516cfc100fd02af8da9bb51172e510f376
Ismael Deka <ismael.deka7899@gmail.com>
2023-04-08 19:39:04 -0400

    first commit

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ data_transforms = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # normalize images
 ])
 
-data_dir = 'food_images'
+data_dir = 'dataset'
 dataset = ImageFolder(root=data_dir, transform=data_transforms)
 
 resnet50 = models.resnet50(pretrained=True)
+
+print(resnet50)
